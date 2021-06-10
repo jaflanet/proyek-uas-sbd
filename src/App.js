b9d2c928302bf3104b46e5fac29168f8f197a030
omar <muhammadjendral12@gmail.com>
2021-06-10 20:49:47 +0700

    paling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import './App.css'
 import {Navbar} from './component'
 import {Footer} from './component'
+import {CreateAccont} from './component'
 import { HomePages,
   HomeValoPages,
   HomePUBGPages,
@@ -11,46 +12,50 @@ function App() {
 const [ada,setAda] = useState(false)
 
 if (ada === false){
-  return(
-     <><div className="Container">
-            <div className="Wrapper">
-            <div class="split left">
-                <div class="centered">
-                    <span className="LOGO_FM">
-                    </span>
-                    <span className="InfoText">
-                        FINDMATES is an online third party website dedicated<br />
-                        to help gamers find teamates, and maybe a soulmate too.
-                    </span>
-                </div>
-            </div>
-            <div class="split right">
-                <div class="centered2">
-                    <div className="LoginPop">
-                        <h1>SIGN IN</h1>
-                        <form method="post">
-                            <div class="txt_field">
-                                <input type="text" required />
-                                <label>USERNAME</label>
-                            </div>
-                            <div class="txt_field">
-                                <input type="password" required />
-                                <label>PASSWORD</label> 
-                            </div>
-                            <div class="pass">Don't have an account?</div>
-                            <div className="submitButton">
-                                <input type="image" src="https://i.ibb.co/M8bpJzM/Component-2.png" alt="login" onClick={()=>setAda(true)}></input>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div className="aboutUs">
-                about us
-            </div>
-            </div>
-        </div>
-    </>
+  return( 
+      <>
+      <CreateAccont />
+      </>
+   // <> 
+    // <div className="Container">
+    //         <div className="Wrapper">
+    //         <div class="split left">
+    //             <div class="centered">
+    //                 <span className="LOGO_FM">
+    //                 </span>
+    //                 <span className="InfoText">
+    //                     FINDMATES is an online third party website dedicated<br />
+    //                     to help gamers find teamates, and maybe a soulmate too.
+    //                 </span>
+    //             </div>
+    //         </div>
+    //         <div class="split right">
+    //             <div class="centered2">
+    //                 <div className="LoginPop">
+    //                     <h1>SIGN IN</h1>
+    //                     <form method="post">
+    //                         <div class="txt_field">
+    //                             <input type="text" required />
+    //                             <label>USERNAME</label>
+    //                         </div>
+    //                         <div class="txt_field">
+    //                             <input type="password" required />
+    //                             <label>PASSWORD</label> 
+    //                         </div>
+    //                         <div class="pass">Don't have an account?</div>
+    //                         <div className="submitButton">
+    //                             <input type="image" src="https://i.ibb.co/M8bpJzM/Component-2.png" alt="login" onClick={()=>setAda(true)}></input>
+    //                         </div>
+    //                     </form>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //         <div className="aboutUs">
+    //             about us
+    //         </div>
+    //         </div>
+    //    </div> 
+    // </>
   )
 }
 else {
