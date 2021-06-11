a2110345333f9d2351c1a3e9a5342c3ef001840a
karen <kareninakamila@gmail.com>
2021-06-11 23:04:30 +0700

    login backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import FMLogo from "./assets/LoginAsset/Component_1.png"
 import './component/Login/index.css'
 import {Navbar} from './component'
 import {Footer} from './component'
 import {CreateAccont} from './component'
+import axios from 'axios';
 import { HomePages,
   HomeValoPages,
   HomePUBGPages,
@@ -11,6 +12,37 @@ import { HomePages,
   import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
 const [ada,setAda] = useState(false)
+
+//buat hubungin ke backend
+//sans rakor aja dlu
+
+  const [ username, setUsername ] = useState("")
+  const [ password, setPassword ] = useState("");
+  // const [loginStatus, setLoginStatus] = usestate("")
+
+
+const login = async() => {
+await axios.post("http://localhost:6970/login/", 
+      {
+        username : username,
+        password : password
+      }).then((response) => {
+        console.log(response);
+        })
+      
+      .then(
+        alert('post successful')
+      )
+    
+      .then(
+        setAda(true)
+      )
+      // then(
+      //   window.location = "http://localhost:3000/recipes"
+      // )
+      // console.log(response)
+  }
+
 
 if (ada === false){
 
@@ -30,22 +62,24 @@ if (ada === false){
                      </span>
                  </div>
              </div>
+        
             <div class="split right">
                  <div class="centered2">
                      <div className="LoginPop">
                          <h1>SIGN IN</h1>
                          <form method="post">
                              <div class="txt_field">
-                                <input type="text" required />
+                                <input type="text" required onChange={(e) => setUsername(e.target.value)}/>
                                  <label>USERNAME</label>
                              </div>
                            <div class="txt_field">
-                                <input type="password" required />
+                                <input type="password" required onChange={(e) => setPassword(e.target.value)}/>
                                  <label>PASSWORD</label> 
                             </div>
                              <div class="pass">Don't have an account?</div>
                              <div className="submitButton">
-                                 <input type="image" src="https://i.ibb.co/M8bpJzM/Component-2.png" alt="login" onClick={()=>setAda(true)}></input>
+                                 {/* <input type="image" src="https://i.ibb.co/M8bpJzM/Component-2.png" alt="login" onClick={()=>setAda(true)}></input> */}
+                                 <input type="image" src="https://i.ibb.co/M8bpJzM/Component-2.png" alt="login" onClick={()=>login()}></input>
                              </div>
                          </form>
                     </div>
@@ -70,6 +104,7 @@ else {
             <Footer /> 
             </Router>
     )
-}
   }
+}
+
 export default App;
