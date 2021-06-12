e7ae82e93bb0ebfb0be262c3f81b7673698519b5
karen <kareninakamila@gmail.com>
2021-06-13 01:57:30 +0700

    select homepage

--- src/component/HomePUBG/index.js
+++ src/component/HomePUBG/index.js
@@ -1,57 +1,117 @@
-// import React from 'react'
-// import gambar_profil from "../../assets/HomePUBG/profil_pubg.png"
-// import player from "../../assets/HomePUBG/recent_pubg.png"
-// import online from "../../assets/HomePUBG/online_pubg.png"
-// import logo from "../../assets/HomePUBG/pubg_logo_white.png"
-// import "./index.css"
-// import axios from 'axios';
+import React from 'react'
+import tabel1 from "../../assets/HomeML/profile-.png"
+import tabel2 from "../../assets/HomeML/profil_profil.png"
+import "./index.css"
+import Select from 'react-select'
+const time_options = [
+    { value: 'Morning', label: 'Morning'},
+    { value: 'Afternoon', label: 'Afternoon'},
+    { value: 'Evening', label: 'Evening'},
+    { value: 'Night', label: 'Night'}
+]
 
-// const HomePUBG= () => {
-//     return (
-//         <div className="con-PUBG">
-//         <div className="warna">
-//         <div className="background-FM">
-               
-//                 <div className="container-row">
+const rank_option = [
+    { value: 'Bronze', label: 'Bronze'},
+    { value: 'Silver', label: 'Silver'},
+    { value: 'Gold', label: 'Gold'},
+    { value: 'Platinum', label: 'Platinum'},
+    { value: 'Diamond', label: 'Epic'},
+    { value: 'Crown', label: 'Crown'},
+    { value: 'Ace', label: 'Ace'},
+    { value: 'Conqueror', label: 'Conqueror'}
+]
+const customStyles = {
+    container: provided => ({
+      ...provided,
+      width: 430,
+      color: 'black'
+    })
+  };
+
+const HomePUBG= () => {
+    return (
+        <>
+        <div className="HomePUBG">
+            <div className="warna-ml">
+                <div className="row1">
+                    <img src={tabel1} className="tab1"/>
+                    <div className="col1">
+                        <div className="tulisancol1">
+                            <label for="Rank">Rank</label>
+                        
+                        <div className="radio1">
+                            <div>
+                                
+                            <br></br>
+                            <Select 
+                            options={rank_option}
+                            styles={customStyles}
+                            isSearchable={true}
+                            
+                            />
+                            <label>RANK</label> 
+                        </div>
+                       </div> 
+                    </div>
+                    <div className="col2">
+                    <div className="tulisancol2">
+                        
+                            <label for="Online">Online Time</label>
+
+                            <div className="radio2">
+                            <br></br>
+                            <Select 
+                            options={time_options}
+                            styles={customStyles}
+                            isSearchable={true}
+                            />
+                            <label>REGION</label> 
+                                 
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className="col3">
+                    <div className="tulisancol3">
+                            <label for="Username">Username Web</label>
+                            
+                            <div className="formulir1">
+                            <br></br>
+                            <br></br>
+                            <input type="form" id="formulir" name="formu"/>
+                            <br></br>
+                             <label for="formulir">Input Your Username</label> 
+                             </div>
+                        </div>
+                        
+                    </div>
                     
-//                         <div className="gambar-profil">
-//                             <img src={gambar_profil} className="profilPUBG"/> 
-//                             </div>
-//                         <div className="logoPUBG">
-//                             <img src={logo} className="PUBG"/>
-                            
-//                         </div>
-//                             <div className="tulisan">
-                            
-//                              <label for="fname">Username :</label>
-//                                 <br></br>
-//                                 <br></br>
-//                                 <label for="lname">ID :</label>
-//                                 <br></br>
-//                                 <br></br>
-//                                 <label for="email">Role :</label>  
-//                             </div>       
-                                               
-                                      
-//                 </div>
-//                <div className="container-row">
-                   
-//                        <div className="recentplayer">
-//                            <img src={player} className="player-column1"/>
-//                        </div>
-//                        <div className="playeronline">
-//                            <img src={online} classname="online-column2"/>
-//                        </div>
-//                    </div>
-                   
-               
+                </div>
+                <div className="row2">
+                    <div className="kotakbawah">
+                    <img src={tabel2} className="tab2"/>
+                    </div>
+                    <div className="tulisanr2">
+                    <label for="tamates">Temates For You</label>
+                    </div>
+                    <div className="r2c1">
+
+                        
+                    </div>
+                    <div className="r2c2">
+                        
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+        </div>
+    
+            
         
-//             </div>
-//         </div>
+        </>
+    )
+    
+}
 
-                          
-//         </div>
-//     )
-// }
-
-// export default HomePUBG
+export default HomePUBG;
