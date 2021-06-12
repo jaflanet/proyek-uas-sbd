89c6af7cc4aefb26bc4b121b76b00c1f42c2197a
RLoo <jonathanfaren@gmail.com>
2021-06-13 00:10:23 +0700

    test modal

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -33,7 +33,7 @@ const Navbar = () => {
           </NavBtnLink>
         </NavMenu>
         <NavBtn>
-          <NavBtnLink to='/ProfilePages'>profil</NavBtnLink>
+          <NavBtnLink to='/SignUpPages'>profil</NavBtnLink>
         </NavBtn>
       </Nav>
     </>
