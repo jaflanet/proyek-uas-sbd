316789cdf8a8d33af54244452ca63a51533bd69f
omar <muhammadjendral12@gmail.com>
2021-06-13 01:21:46 +0700

    hayii

--- src/component/HomeML/index.js
+++ src/component/HomeML/index.js
@@ -1,79 +1,112 @@
-import React, { useState,useEffect } from 'react'
-import gambar_profil from "../../assets/HomeML/profil_ml.png"
-import player from "../../assets/HomeML/recent_ml.png"
-import online from "../../assets/HomeML/online_ml.png"
-import logo from "../../assets/HomeML/ml_logo_white.png"
+import React from 'react'
+import tabel1 from "../../assets/HomeML/profile-.png"
+import tabel2 from "../../assets/HomeML/profil_profil.png"
 import "./index.css"
-import { useParams } from 'react-router-dom'
-import axios from 'axios';
+
 
 const HomeML= () => {
-    const { username } = useParams()
-    const [data,setData] = useState()
-    useEffect(() => {
-        let toggle= true
-        const fetchdata =async()=>{
-            const response = await axios.get(`http://localhost:6970/signup/${username}`)
-            if( toggle === true) {
-                console.log(response)
-                // setData(response.data)
-                toggle = false
-            }
-
-        }
-        fetchdata();
-        return ()=> toggle = false
-    })
-    
-   
-
-    
     return (
-        <div className="con-ML">
-        <div className="warna">
-        <div className="background-FM">
-               
-                <div className="container-row">
-                    
-                        <div className="gambar-profil">
-                            <img src={gambar_profil} className="profilml"/> 
-                            </div>
-                        <div className="logoml">
-                            <img src={logo} className="ml"/>
+        <>
+        <div className="HomeML">
+            <div className="warna-ml">
+                <div className="row1">
+                    <img src={tabel1} className="tab1"/>
+                    <div className="col1">
+                        <div className="tulisancol1">
+                            <label for="Rank">Rank</label>
+                        
+                        <div className="radio1">
+                            <div>
+                                
+                            <br></br>
                             
+                            <input type="radio" id="Rank" name="ranking"/>
+                            <label for="Rank">Warrior</label>
+                            <br></br>
+                            <input type="radio" id="Rank" name="ranking"/>
+                            <label for="Rank">Elite</label>
+                            <br></br>
+                            <input type="radio" id="Rank" name="ranking"/>
+                            <label for="Rank">Master</label>
+                            <br></br>
+                            <input type="radio" id="Rank" name="ranking"/>
+                            <label for="Rank">Grand Master</label>
+                            <br></br>
+                            <input type="radio" id="Rank" name="ranking"/>
+                            <label for="Rank">Epic</label>
+                            <br></br>
+                            <input type="radio" id="Rank" name="ranking"/>
+                            <label for="Rank">Lagends</label>
+                            <br></br>
+                            <input type="radio" id="Rank" name="ranking"/>
+                            <label for="Rank">Mythic</label>
+                            <br></br>
+                            <input type="radio" id="Rank" name="ranking"/>
+                            <label for="Rank">Mythic Glory</label>
                         </div>
-                            <div className="tulisan">
-                             <label for="fname">Username :</label>
-                             {/* <p>{data.username}</p> */}
-                                <br></br>
-                                <br></br>
-                                <label for="lname">ID :</label>
-                                <br></br>
-                                <br></br>
-                                <label for="email">Role :</label>  
-                            </div>       
-                                               
-                                      
-                </div>
-               <div className="container-row">
-                   
-                       <div className="recentplayer">
-                           <img src={player} className="player-column1"/>
-                       </div>
-                       <div className="playeronline">
-                           <img src={online} classname="online-column2"/>
-                       </div>
-                   </div>
-                   
-               
-        
-            </div>
-        </div>
+                       </div> 
+                    </div>
+                    <div className="col2">
+                    <div className="tulisancol2">
+                        
+                            <label for="Online">Online Time</label>
 
-                          
+                            <div className="radio2">
+                            <br></br>
+                            <input type="radio" id="cuca" name="cuaca1"/>
+                            <label for="cuca">Morning</label> 
+                            <input type="radio" id="cuca" name="cuaca1"/>
+                            <label for="cuca">Afternoon</label> 
+                            <input type="radio" id="cuca" name="cuaca1"/>
+                            <label for="cuca">Evening</label> 
+                            <input type="radio" id="cuca" name="cuaca1"/>
+                            <label for="cuca">Night</label>   
+                                
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className="col3">
+                    <div className="tulisancol3">
+                            <label for="Username">Username Web</label>
+                            
+                            <div className="formulir1">
+                            <br></br>
+                            <br></br>
+                            <input type="form" id="formulir" name="formu"/>
+                            <br></br>
+                             <label for="formulir">Input Your Username</label> 
+                             </div>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+                <div className="row2">
+                    <div className="kotakbawah">
+                    <img src={tabel2} className="tab2"/>
+                    </div>
+                    <div className="tulisanr2">
+                    <label for="tamates">Temates For You</label>
+                    </div>
+                    <div className="r2c1">
+
+                        
+                    </div>
+                    <div className="r2c2">
+                        
+                    </div>
+                </div>
+            </div>
+            </div>
+            
         </div>
+    
+            
+        
+        </>
     )
     
 }
 
-export default HomeML
+export default HomeML;
