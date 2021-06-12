3fc989acd30d83d33897ed96c8447cf00004a3d0
omar <muhammadjendral12@gmail.com>
2021-06-12 18:14:01 +0700

    prifilenyakelar

--- src/component/Profile/index.js
+++ src/component/Profile/index.js
@@ -1,11 +1,58 @@
 import React from 'react'
-import "./index.css"
+import gambar_profil from "../../assets/ProfileAsset/profil_pubg.png"
+import player from "../../assets/ProfileAsset/profile-.png"
 
-const Profile = () => {
+
+import "./index.css"
+import axios from 'axios';
+
+const Profile= () => {
     return (
-        <>
-          <h1>profile</h1>  
-        </>
+        <div className="con-ML">
+        <div className="warna">
+        <div className="background-FM">
+               
+                <div className="container-row">
+                    
+                        <div className="gambar-profil">
+                            <img src={gambar_profil} className="profilml"/> 
+                            </div>
+                    
+                            <div className="tulisan">
+                            
+                             <label for="fname">Username :</label>
+                                <br></br>
+                                <br></br>
+                                <label for="lname">Age :</label>
+                                <br></br>
+                                <br></br>
+                                <label for="email">Gender :</label>
+                                <br></br>
+                                <br></br>
+                                <label for="regi">Region :</label>
+
+                            </div>  
+                            <div className="tulisan2">
+                                <label for="myprofile">My Profile</label>
+                                
+                            </div>     
+                                               
+                                      
+                </div>
+               <div className="container-row">
+                   
+                       <div className="recentplayer">
+                           <img src={player} className="player-column1"/>
+                       </div>
+                   </div>
+                   
+               
+        
+            </div>
+        </div>
+
+                          
+        </div>
     )
 }
 
